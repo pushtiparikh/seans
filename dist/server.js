76105d4ab59824d8e55743168a99fba34445c61d
Pushti Parikh <98299460+pushtiparikh@users.noreply.github.com>
2023-07-17 20:06:42 +0530

    Add files via upload

---
+++ dist/server.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = __importDefault(require("./app"));
+const port = parseInt(process.env.PORT || '3000');
+const server = new app_1.default().Start(port)
+    .then(port => console.log(`Server running on port ${port}`))
+    .catch(error => {
+    console.log(error);
+    process.exit(1);
+});
+exports.default = server;
